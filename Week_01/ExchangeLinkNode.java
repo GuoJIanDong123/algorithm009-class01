f9d41821da0e8e5e89b1719f63bb2629736ea213
GuoJIanDong123 <35921801+GuoJIanDong123@users.noreply.github.com>
2020-05-24 21:19:16 +0800

    ExchangeLinkNode
    ExchangeLinkNode

---
+++ Week_01/ExchangeLinkNode.java
@@ -0,0 +1,16 @@
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head ==null || head.next ==null) return head;
+       ListNode next = head.next;
+       head.next =  swapPairs(next.next);
+       next.next = head;
+       return next;
+    }
+}
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
