38ba7c37b1b55baf99fdd66108b7742dc4923054
GuoJIanDong123 <35921801+GuoJIanDong123@users.noreply.github.com>
2020-07-19 23:17:44 +0800

    UniqueCharacterinString

---
+++ Week_09/UniqueCharacterinString.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int firstUniqChar(String s) {
+
+        int[] letters = new int[26];
+        for(int i=0;i<s.length();i++){
+            letters[s.charAt(i)-'a']++;
+        }
+        for(int j=0;j<s.length();j++){
+            if(letters[s.charAt(j)-'a']==1){
+                return j;
+            }
+        }
+
+        return -1;
+
+
+    }
+}
